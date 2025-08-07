dd02b0ea57d47c0c712e1f2a5c2b92eaa06f908e
babakou <babakou.1988@gmail.com>
2025-08-07 09:45:08 +0900

    最初のgreetを別スレッドに委譲してUIスレッドと非同期に実行するように変更する。
    この効果を確かめるためにUIスレッドと同期でMsgを変更するgreet2を追加

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,6 +1,18 @@
+use tauri::Emitter;
+
 // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
 #[tauri::command]
-fn greet(name: &str) -> String {
+fn greet_async(app: tauri::AppHandle, name: String) {
+    //format!("Hello, {}! You've been greeted from Rust!", name)
+    std::thread::spawn( move || {
+        let greet = format!("Hello, {}! You've been greeted from Rust!", name);
+        std::thread::sleep(std::time::Duration::from_secs(5));
+        app.emit("greet", greet).unwrap();
+    });
+}
+
+#[tauri::command]
+fn greet2(name: &str) -> String{
     format!("Hello, {}! You've been greeted from Rust!", name)
 }
 
@@ -8,7 +20,7 @@ fn greet(name: &str) -> String {
 pub fn run() {
     tauri::Builder::default()
         .plugin(tauri_plugin_opener::init())
-        .invoke_handler(tauri::generate_handler![greet])
+        .invoke_handler(tauri::generate_handler![greet_async, greet2])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,28 @@
 import { invoke } from "@tauri-apps/api/core";
+import { listen } from "@tauri-apps/api/event";
 
 let greetInputEl: HTMLInputElement | null;
 let greetMsgEl: HTMLElement | null;
+let greetInput2El : HTMLInputElement | null;
+let greetMsg2El : HTMLElement | null;
 
 async function greet() {
   if (greetMsgEl && greetInputEl) {
     // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
-    greetMsgEl.textContent = await invoke("greet", {
+    // greetMsgEl.textContent = await invoke("greet", {
+    //   name: greetInputEl.value,
+    // });
+    await invoke("greet_async", {
       name: greetInputEl.value,
+    });
+  }
+}
+
+async function greet2() {
+  if (greetMsg2El && greetInput2El) {
+    // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
+    greetMsg2El.textContent = await invoke("greet2", {
+      name: greetInput2El.value,
     });
   }
 }
@@ -19,4 +34,17 @@ window.addEventListener("DOMContentLoaded", () => {
     e.preventDefault();
     greet();
   });
+
+  greetInput2El = document.querySelector("#greet-input2");
+  greetMsg2El = document.querySelector("#greet-msg2");
+  document.querySelector("#greet-form2")?.addEventListener("submit", (e) => {
+    e.preventDefault();
+    greet2();
+  });
 });
+
+listen<string>('greet', (greet) => {
+  if (greetMsgEl) {
+    greetMsgEl.textContent = greet.payload;
+  }
+})
