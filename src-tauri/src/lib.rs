17761e795008ae5f1ea4b4d514b763f65c18a1f2
babakou <babakou.1988@gmail.com>
2025-08-07 13:11:50 +0900

    メッセージを非同期に更新する方法を変更 Appの起動時にスレッドを起動しておき、tauriのcommandからmpsc::channelでメッセージを投げて起動するようにする。
    tauri::AppはManager Traitを実装しており、このTraitのmanage()を使うとアプリケーションに状態(属性？)を持たせられる。
    これを使ってSenderをアプリケーションの属性として持たせ、UIスレッドからSenderを使ってバックグラウンドスレッドにメッセージを送るようにした。

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,14 +1,20 @@
-use tauri::Emitter;
+use tauri::{Emitter, Manager};
 
 // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
 #[tauri::command]
 fn greet_async(app: tauri::AppHandle, name: String) {
     //format!("Hello, {}! You've been greeted from Rust!", name)
-    std::thread::spawn( move || {
-        let greet = format!("Hello, {}! You've been greeted from Rust!", name);
-        std::thread::sleep(std::time::Duration::from_secs(5));
-        app.emit("greet", greet).unwrap();
-    });
+    // std::thread::spawn( move || {
+    //     greet_async_sub(app, name);
+    // });
+    let tx : tauri::State<std::sync::mpsc::Sender<String>> = app.state();
+    tx.send(name).unwrap();
+}
+
+fn greet_async_sub(app: &tauri::AppHandle, name: String) {
+    let greet = format!("Hello, {}! You've been greeted from Rust!", name);
+    std::thread::sleep(std::time::Duration::from_secs(3));
+    app.emit("greet", greet).unwrap();
 }
 
 #[tauri::command]
@@ -19,6 +25,18 @@ fn greet2(name: &str) -> String{
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
+        .setup(|app| {
+            let app_handle = app.handle().clone();
+            let (tx, rx) = std::sync::mpsc::channel();
+            let _join_handle = std::thread::spawn( move || {
+                loop {
+                    let received = rx.recv().unwrap();
+                    greet_async_sub(&app_handle, received);
+                }
+            });
+            app.manage(tx);
+            Ok(())
+        })
         .plugin(tauri_plugin_opener::init())
         .invoke_handler(tauri::generate_handler![greet_async, greet2])
         .run(tauri::generate_context!())
